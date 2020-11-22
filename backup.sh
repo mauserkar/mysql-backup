6b5b49aa3c07d818c7a199849282bbfc0e45f6ef
Carlos Garcia <c.garcia@whisbi.com>
2020-11-23 00:49:32 +0100

    fix error loop

--- backup.sh
+++ backup.sh
@@ -7,7 +7,7 @@ find $BAK_OUTPUT -mindepth 1 -maxdepth 1 -mtime +$BAK_RETENTION -exec rm -rf {} 
 
 DATABASES=$(mysql --host=$MYSQL_SERVER --user=$MYSQL_USER --password=$MYSQL_PASSWORD -e "SHOW DATABASES;" | tr -d "| " | grep -v Database)
 
-for $DB in $DATABASES; do
+for DB in $DATABASES; do
     if ["$DB" != "performance_schema" ] && [ "$DB" != "information_schema" ] && [ "$DB" != _* ] && [ "$DB" != "mysql" ]; then
         echo "Dumping database: $DB"
         mysqldump --force --opt --host=$MYSQL_SERVER --user=$MYSQL_USER --password=$MYSQL_PASSWORD --databases $DB > $BAK_OUTPUT/$DB-$BAK_DATE.sql
