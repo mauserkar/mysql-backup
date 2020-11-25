5c2f907fd19f5f1ad072db5cf2c474719a75edd0
Carlos Garcia <c.garcia@whisbi.com>
2020-11-26 00:08:14 +0100

    update backup

--- backup.sh
+++ backup.sh
@@ -12,7 +12,7 @@ fi
 for DB in $(echo $MYSQL_DATABASES | tr -d \' | tr -d \"); do
     if [ "$DB" != "performance_schema" ] && [ "$DB" != "information_schema" ] && [ "$DB" != _* ] && [ "$DB" != "mysql" ]; then
         echo "$BAK_DATE Dumping database: $DB"
-	    mysqldump --force --opt --host=$MYSQL_SERVER--user=$MYSQL_USER --password=$MYSQL_PASSWORD --databases $DB > $BAK_OUTPUT/$DB-$BAK_DATE.sql
+	    mysqldump --force --opt --host=$MYSQL_SERVER --user=$MYSQL_USER --password=$MYSQL_PASSWORD --databases $DB > $BAK_OUTPUT/$DB-$BAK_DATE.sql
 	    gzip $BAK_OUTPUT/$DB-$BAK_DATE.sql
     fi
 done
