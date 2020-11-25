236bded5e16c7e621802761a9e5f1ad088798d11
Carlos Garcia <c.garcia@whisbi.com>
2020-11-25 22:53:39 +0100

    fix quotes

--- backup.sh
+++ backup.sh
@@ -9,10 +9,15 @@ if [ -z "$MYSQL_DATABASES" ]; then
     MYSQL_DATABASES=$(mysql --host=$MYSQL_SERVER --user=$MYSQL_USER --password=$MYSQL_PASSWORD -e "SHOW DATABASES;" | tr -d "| " | grep -v Database)
 fi
 
-for DB in  ${MYSQL_DATABASES//\"/}; do
+for DB in $(echo $MYSQL_DATABASES | tr -d \' | tr -d \"); do
     if [ "$DB" != "performance_schema" ] && [ "$DB" != "information_schema" ] && [ "$DB" != _* ] && [ "$DB" != "mysql" ]; then
         echo "$BAK_DATE Dumping database: $DB"
-	mysqldump --force --opt --host=$MYSQL_SERVER --user=$MYSQL_USER --password=$MYSQL_PASSWORD --databases $DB > $BAK_OUTPUT/$DB-$BAK_DATE.sql
-	gzip $BAK_OUTPUT/$DB-$BAK_DATE.sql
+	    mysqldump --force --opt 
+                  --host=$MYSQL_SERVER \
+                  --user=$MYSQL_USER \ 
+                  --password=$MYSQL_PASSWORD \ 
+                  --databases $DB > $BAK_OUTPUT/$DB-$BAK_DATE.sql || \
+                  echo "$BAK_DATE Fail Dumping database: $DB"
+	    gzip $BAK_OUTPUT/$DB-$BAK_DATE.sql
     fi
 done
