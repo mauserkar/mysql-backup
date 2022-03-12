e7a96eb35bd550fde76110604e5b6e166db8e0b6
Carlos Garcia <carlos.garcia@scrm.lidl>
2022-03-12 09:33:54 +0100

    update version 1.0

--- backup.sh
+++ backup.sh
@@ -5,12 +5,12 @@ BAK_DATE=$(date "+%d%m%Y-%H%M%S")
 
 find $BAK_OUTPUT -mindepth 1 -maxdepth 1 -mtime +$BAK_RETENTION -exec rm -rf {} \;
 
-if [ -z "$MYSQL_DATABASES" ]; then
+if [-z "$MYSQL_DATABASES"] || ["$MYSQL_DATABASES" == ""]; then
     MYSQL_DATABASES=$(mysql --host=$MYSQL_SERVER --user=$MYSQL_USER --password=$MYSQL_PASSWORD -e "SHOW DATABASES;" | tr -d "| " | grep -v Database)
 fi
 
 for DB in $(echo $MYSQL_DATABASES | tr -d \' | tr -d \"); do
-    if [ "$DB" != "performance_schema" ] && [ "$DB" != "information_schema" ] && [ "$DB" != _* ] && [ "$DB" != "mysql" ]; then
+    if ["$DB" != "performance_schema"] && ["$DB" != "information_schema"] && ["$DB" != _*] && ["$DB" != "mysql"]; then
         echo "$BAK_DATE Dumping database: $DB"
 	    mysqldump --force --opt --host=$MYSQL_SERVER --user=$MYSQL_USER --password=$MYSQL_PASSWORD --databases $DB > $BAK_OUTPUT/$DB-$BAK_DATE.sql
 	    gzip $BAK_OUTPUT/$DB-$BAK_DATE.sql
